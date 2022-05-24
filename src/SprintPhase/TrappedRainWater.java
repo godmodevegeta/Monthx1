10b3299f6c5f12b5ea9722b01ab5ffa26d4add25
godmodenorfin <kshubham1904@gmail.com>
2022-05-24 09:22:52 +0530

    all actions commited

---
+++ src/SprintPhase/TrappedRainWater.java
@@ -0,0 +1,29 @@
+package SprintPhase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class TrappedRainWater {
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 2, 1 };
+        Stack stack = new Stack<>();
+        s(0, arr, stack);
+        
+
+
+    }
+
+    private static void s(int i, int[] arr, Stack stack) {
+        if (i == arr.length) {
+            System.out.println(stack);
+            return;
+        }
+        
+        s(i + 1, arr, stack);
+        stack.add(arr[i]);
+        s(i + 1, arr, stack);
+        // stack.pop();
+    }
+}
