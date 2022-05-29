bebfb9c547e44393fc985490a8357359be6e4910
godmodenorfin <kshubham1904@gmail.com>
2022-05-29 12:37:09 +0530

    some changes in feature-01

--- src/App.java
+++ src/App.java
@@ -1,8 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        TreeNode head = new TreeNode();
-        head.left = head.right;
         
+        Integer f = new Integer(5);
+        int g = 5;
+        System.out.println(g != f);
+        System.out.println(String.valueOf(5) == String.valueOf(7));
+        System.out.println((char)('0' + 0));
+        System.out.println((int)('5' - '0'));
 
     }
 }
